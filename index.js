f86892e20a75505e2731c9f25e7146a44f774e38
angelrosmend <angelrosmend@gmail.com>
2020-10-27 20:40:39 -0300

    splitting reducer

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
+function buyIceCream(){
+    return{
+        type: BUY_ICECREAM
+    }
+}
 function buyCake() {
     return {
         type: BUY_CAKE,
@@ -12,19 +18,62 @@ function buyCake() {
 
 // (prevState, action) => newState
 
-const initialState = {
-    numOfCakes: 10
-}
+/* const initialState = {
+    numOfCakes: 10,
+    numOfIceCream: 20
+} */
 
-const reducer = (state = initialState, action) =>  {
+/* const reducer = (state = initialState, action) =>  {
     switch(action.type) {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
         }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
+        default: return state
+    }
+} */
+
+const initialStateCakes = {
+    numOfCakes: 10
+}
+
+const initialStateIceCream = {
+    numOfIceCream: 20
+}
+
+const reducerCakes = (state = initialStateCakes, action) =>  {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
         default: return state
     }
 }
+
+
+const reducerIceCream = (state = initialStateIceCream, action) =>  {
+    switch(action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
+        default: return state
+    }
+}
+
+
+
+
+
 
 const store = createStore(reducer)
 console.log('initial state', store.getState())
@@ -32,4 +81,11 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+
+
 unsubscribe()
