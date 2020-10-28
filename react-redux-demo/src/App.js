195b0d63f5413c224161328a33c973c5057e6d5c
angelrosmend <angelrosmend@gmail.com>
2020-10-28 16:25:20 -0300

    React-redux fundamentals - first app

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import store from './redux/store'
+import { Provider } from 'react-redux'
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer/>
+      </div>
+    </Provider>
   );
 }
 
