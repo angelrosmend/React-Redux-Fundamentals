daff42661e83ab2540c841c8c590ba1a96f52831
angelrosmend <angelrosmend@gmail.com>
2020-10-27 21:08:26 -0300

    Redux-logger & Middlewares fundamentals

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -10,33 +14,14 @@ function buyIceCream(){
         type: BUY_ICECREAM
     }
 }
-function buyCake() {
+
+function buyCake(){
     return {
         type: BUY_CAKE,
         info: 'First redux action'
     }
 }
 
-// (prevState, action) => newState
-
-/* const initialState = {
-    numOfCakes: 10,
-    numOfIceCream: 20
-} */
-
-/* const reducer = (state = initialState, action) =>  {
-    switch(action.type) {
-        case BUY_CAKE: return {
-            ...state,
-            numOfCakes: state.numOfCakes - 1
-        }
-        case BUY_ICECREAM: return {
-            ...state,
-            numOfIceCream: state.numOfIceCream - 1
-        }
-        default: return state
-    }
-} */
 
 const initialStateCakes = {
     numOfCakes: 10
@@ -70,9 +55,9 @@ const rootReducer = combineReducers({
     cake: reducerCakes,
     iceCream: reducerIceCream
 })
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('initial state', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
@@ -81,4 +66,27 @@ store.dispatch(buyCake())
 store.dispatch(buyIceCream())
 store.dispatch(buyIceCream())
 store.dispatch(buyIceCream())
-.unsubscribe()
+unsubscribe()
+
+
+
+// (prevState, action) => newState
+
+/* const initialState = {
+    numOfCakes: 10,
+    numOfIceCream: 20
+} */
+
+/* const reducer = (state = initialState, action) =>  {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
+        default: return state
+    }
+} */
