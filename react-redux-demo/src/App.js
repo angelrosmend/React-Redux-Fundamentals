f8a3c47a3a8f660a4ee56539e457f379ff2ee1b9
angelrosmend <angelrosmend@gmail.com>
2020-10-28 17:26:35 -0300

    adding more reducers and using  combineReducers on react-redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import store from './redux/store'
 import { Provider } from 'react-redux'
-import CakeContainer from './components/CakeContainer';
+import CakeContainer from './components/CakeContainer.jsx';
+import HooksCakeContainer from './components/HooksCakeContainer.jsx';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer/>
+        <HooksCakeContainer/>
+        <IceCreamContainer/>
       </div>
     </Provider>
   );
---
+++ react-redux-demo/src/redux/icecream/icecreamActions.js
@@ -0,0 +1,8 @@
+import { BUY_CAKE } from '../cakes/cakeTypes'
+import {BUY_ICECREAM} from './icecreamTypes'
+
+export const buyIceCream = () => {
+    return {
+        type: BUY_ICECREAM
+    }
+}
